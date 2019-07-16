79e204a6339ebdfb18337c444bdeeee94a58c313
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-07-16 18:38:33 -0400

    Add get_packages tests for limit and return format

--- R/get_packages.R
+++ R/get_packages.R
@@ -4,9 +4,30 @@
 #'
 #' @export
 get_packages <- function(limit = 31) {
+  limit <- check_limit(limit)
+
   packages <- ckanr::package_list_current(limit = limit, url = opendatatoronto_ckan_url, as = "table")
 
   packages_relevant_cols <- packages[, c("title", "id", "topics", "excerpt", "dataset_category", "formats", "refresh_rate")]
 
   tibble::as_tibble(packages_relevant_cols)
+}
+
+check_limit <- function(limit) {
+  if (length(limit) != 1) {
+    stop("`limit` must be a length 1 positive integer vector.",
+      call. = FALSE
+    )
+  }
+  else if (!is.numeric(limit) ||
+    !(limit %% 1 == 0) ||
+    limit <= 0 ||
+    limit == Inf) {
+    stop("`limit` must be a positive integer.",
+      call. = FALSE
+    )
+  }
+  else {
+    limit
+  }
 }
---
+++ tests/testthat/test-get_packages.R
@@ -0,0 +1,37 @@
+context("test-get_packages")
+
+test_that("get_packages returns error if limit is not a length 1 positive integer.", {
+  expect_error(
+    get_packages(limit = -1),
+    "`limit` must be a positive integer."
+  )
+  expect_error(
+    get_packages(limit = 0),
+    "`limit` must be a positive integer."
+  )
+  expect_error(
+    get_packages(limit = -Inf),
+    "`limit` must be a positive integer."
+  )
+  expect_error(
+    get_packages(limit = 1.2),
+    "`limit` must be a positive integer."
+  )
+  expect_error(
+    get_packages(limit = c(1, 2)),
+    "`limit` must be a length 1 positive integer."
+  )
+})
+
+test_that("get_packages returns the right output formats.", {
+  skip_on_cran()
+  output <- get_packages(1)
+  expect_is(output, "tbl_df")
+  expect_is(output$title, "character")
+  expect_is(output$id, "character")
+  expect_is(output$topics, "character")
+  expect_is(output$excerpt, "character")
+  expect_is(output$dataset_category, "character")
+  expect_is(output$formats, "list")
+  expect_is(output$refresh_rate, "character")
+})
