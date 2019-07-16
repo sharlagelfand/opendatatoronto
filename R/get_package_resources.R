9c8a75da62fc5f83bc3a916b7ed4ab5b549e8ee2
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-07-16 19:28:17 -0400

    Add get_package_resources tests for package_id, package_id not found, and return format

--- R/get_package_resources.R
+++ R/get_package_resources.R
@@ -3,7 +3,14 @@
 #' @param package_id Package ID
 #' @export
 get_package_resources <- function(package_id) {
-  package_res <- ckanr::package_show(id = package_id, url = "https://ckan0.cf.opendata.inter.sandbox-toronto.ca/", as = "table")
+  package_id <- check_package_id(package_id)
+
+  package_res <- try(
+    ckanr::package_show(id = package_id, url = opendatatoronto_ckan_url, as = "table"),
+    silent = TRUE
+  )
+
+  package_res <- check_package_show_results(package_res)
 
   if (package_res[["num_resources"]] == 0) {
     tibble::tibble(
@@ -22,5 +29,32 @@ get_package_resources <- function(package_id) {
     res[["url"]] <- ifelse(res[["datastore_active"]], paste0("https://ckan0.cf.opendata.inter.sandbox-toronto.ca/download_resource/", res[["id"]]), res[["url"]])
 
     tibble::as_tibble(res[, c("name", "id", "format", "created", "url")])
+  }
+}
+
+check_package_id <- function(package_id) {
+  if (!is.character(package_id)) {
+    stop("`package_id` must be a character vector.",
+      call. = FALSE
+    )
+  }
+  else if (length(package_id) != 1) {
+    stop("`package_id` must be a length 1 character vector.",
+      call. = FALSE
+    )
+  }
+  else {
+    package_id
+  }
+}
+
+check_package_show_results <- function(res) {
+  if (class(res) == "try-error" && grepl("404", res)) {
+    stop("`package_id` was not found.",
+      call. = FALSE
+    )
+  }
+  else {
+    res
   }
 }
---
+++ tests/testthat/test-get_package_resources.R
@@ -0,0 +1,30 @@
+context("test-get_package_resources")
+
+test_that("get_package_resources returns an error if package_id is not a length 1 character vector", {
+  expect_error(
+    get_package_resources(1234),
+    "must be a character vector"
+  )
+  expect_error(
+    get_package_resources(c("1234", "5678")),
+    "must be a length 1 character vector"
+  )
+})
+
+test_that("get_package_resources returns an error if the package_id can't be found.", {
+  expect_error(
+    get_package_resources("1234"),
+    "not found"
+  )
+})
+
+test_that("get_package_resources returns the right output formats.", {
+  skip_on_cran()
+  output <- get_package_resources("680d4f11-9bb2-4efe-9b13-50b34d8eba10")
+  expect_is(output, "tbl_df")
+  expect_is(output$name, "character")
+  expect_is(output$id, "character")
+  expect_is(output$format, "character")
+  expect_is(output$created, "date")
+  expect_is(output$url, "character")
+})
