2373f1e868508278eaa5d18f58ec6782e19b3f05
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-07-18 22:00:42 -0400

    get_resource into session only, no download

--- R/get_resource.R
+++ R/get_resource.R
@@ -2,8 +2,6 @@
 #'
 #' @param url URL of the resource
 #' @param format Format of the resource
-#' @param store One of session (default) or disk. session stores in R session, and disk saves the file to disk.
-#' @param path If store = disk, you must give a path to store the file to
 #'
 #' @export
 #' @examples
@@ -11,9 +9,10 @@
 #' res <- list_package_resources("832a5967-15a8-4dce-aad5-36d2d45f9d04")
 #' get_resource(url = res[1, "url"], format = res[1, "format"])
 #' }
-get_resource <- function(url, format, store = "session", path = "file") {
+get_resource <- function(url, format) {
   format <- check_format(format)
 
-  res <- ckanr::ckan_fetch(x = url, store = store, path = path, format = format)
+  res <- ckanr::ckan_fetch(x = url, store = "session", format = format)
+
   tibble::as_tibble(res)
 }
