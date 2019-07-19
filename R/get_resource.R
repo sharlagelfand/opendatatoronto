34b26e1ceaf0197f426a5082f60afaa19cd71ede
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-07-18 21:57:05 -0400

    Move GeoJSON handling to feature branch

--- R/get_resource.R
+++ R/get_resource.R
@@ -6,27 +6,14 @@
 #' @param path If store = disk, you must give a path to store the file to
 #'
 #' @export
-#' @examples \dontrun{
+#' @examples
+#' \dontrun{
 #' res <- list_package_resources("832a5967-15a8-4dce-aad5-36d2d45f9d04")
-#' list_resource(url = res[1, "url"], format = res[1, "format"])
+#' get_resource(url = res[1, "url"], format = res[1, "format"])
 #' }
 get_resource <- function(url, format, store = "session", path = "file") {
   format <- check_format(format)
 
-  if (format == "GEOJSON") {
-    query <- list(format = "geojson", projection = "4326")
-  }
-  else {
-    query <- NULL
-  }
-
-  res <- ckanr::ckan_fetch(x = url, store = store, path = path, format = format, args = query)
-
-  if (format == "GEOJSON") {
-    res$features <- tibble::as_tibble(res$features)
-    res
-  }
-  else {
-    tibble::as_tibble(res)
-  }
+  res <- ckanr::ckan_fetch(x = url, store = store, path = path, format = format)
+  tibble::as_tibble(res)
 }
