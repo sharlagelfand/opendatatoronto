0be2926ef505c2c24269b1f761c1ae837fada110
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-24 16:21:11 -0400

    Fix error

--- R/get_resource.R
+++ R/get_resource.R
@@ -42,7 +42,7 @@ get_resource <- function(resource) {
 check_format <- function(format) {
   format <- toupper(format)
   if (!(format %in% c("CSV", "XLS", "XLSX", "XML", "JSON", "SHP", "ZIP", "GEOJSON"))) {
-    stop(paste(format, "`format` can't be downloaded via package; please visit Open Data Portal directly to download. \n Supported `format`s are: CSV, XLS, XLSX, XML, JSON, SHP, ZIP", "GEOJSON"),
+    stop(paste(format, "`format` can't be downloaded via package; please visit Open Data Portal directly to download. \n Supported `format`s are: CSV, XLS, XLSX, XML, JSON, SHP, ZIP, GEOJSON."),
       call. = FALSE
     )
   }
