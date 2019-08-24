b51f9e87a0f0e0a585d5b3cf6495399b42c51181
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-08-24 16:03:11 -0400

    Handle case when package searched does not have all fields

--- R/search_packages.R
+++ R/search_packages.R
@@ -11,8 +11,7 @@
 search_packages <- function(title, limit = 1000) {
   packages <- ckanr::package_search(fq = paste0("title:", '"', title, '"'), rows = limit, url = opendatatoronto_ckan_url, as = "table")
 
-  if (length(packages[["results"]]) == 0) {
-    tibble::tibble(
+  res_init <- tibble::tibble(
       title = character(),
       id = character(),
       topics = character(),
@@ -20,10 +19,22 @@ search_packages <- function(title, limit = 1000) {
       dataset_category = character(),
       formats = character(),
       refresh_rate = character(),
-      num_resource = character()
+      num_resources = integer()
     )
+
+  if (length(packages[["results"]]) == 0) {
+    res_init
   } else {
     res <- tibble::as_tibble(packages[["results"]])
+    res <- res[, names(res) %in% names(res_init)]
+    for(i in names(res_init)) {
+      if(is.null(res[[i]])) {
+        res[[i]] <- switch(class(res_init[[i]]),
+                           character = NA_character_,
+                           integer = NA_integer_)
+      }
+    }
+    res <- rbind(res_init, res)
     res[, c("title", "id", "topics", "excerpt", "dataset_category", "formats", "refresh_rate", "num_resources")]
   }
 }
