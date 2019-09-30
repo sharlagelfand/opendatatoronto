23f559e271c006b55c275ae53017a3bd26c1041f
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-09-29 20:39:31 -0400

    Add package doc

---
+++ R/opendatatoronto.R
@@ -0,0 +1,9 @@
+#' opendatatoronto
+#'
+#' opendatatoronto is an R interface to the \href{https://portal0.cf.opendata.inter.sandbox-toronto.ca/}{City of Toronto Open Data Portal}.
+#' The goal of the package is to help read data directly into R without needing
+#' to manually download it via the portal.
+#'
+#' For more information, please start with the vignettes: \code{browseVignettes(package = "opendatatoronto")} or visit the \href{https://sharlagelfand.github.io/opendatatoronto/}{package website}.
+#'
+"_PACKAGE"
